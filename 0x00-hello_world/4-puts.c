00dcabac722ee42265470143989796ce8420d28b
khalidhub7 <louguikhalid7@gmail.com>
2023-08-12 07:13:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+includ < stdio.h >
+/*
+ *main -entry point
+ *
+ *description : a C program with puts function
+ *
+ * always 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
