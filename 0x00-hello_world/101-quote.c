8ee221baf1df59b724faccd3aad8e51e74ec506d
khalidhub7 <louguikhalid7@gmail.com>
2023-08-13 01:45:55 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main -entry point
  *
- * Description: a C program that prints exactly ...
+ * Description: a C program that prints exactly ... without printf and put
  *
  * Return: (1) (success)
  *
 */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quo,59);
 	return (1);
 }
