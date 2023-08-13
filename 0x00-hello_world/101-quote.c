7d0d65dcee40bf009d6e83e2109746c0e022c074
khalidhub7 <louguikhalid7@gmail.com>
2023-08-13 01:09:48 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * description : a C program that prints exactly ...
+ *
+ * Return : 1 (success)
+ *
+*/ 
+int main (void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
