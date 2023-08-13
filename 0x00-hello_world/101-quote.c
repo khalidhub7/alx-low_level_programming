d6314dc4cd9c730fbaad7bf9902fc561b6571867
khalidhub7 <louguikhalid7@gmail.com>
2023-08-13 01:50:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,11 @@
  * Return: (1) (success)
  *
 */
+
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1,quo,59);
+
+	write(1, quo, 59);
 	return (1);
 }
