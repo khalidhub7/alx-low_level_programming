9f6dc3d0d4e590ea70b1c77e8c3266ff90ee9ebf
khalidhub7 <louguikhalid7@gmail.com>
2023-08-16 10:34:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,12 +2,13 @@
 	/**
 	 * main - print _putchar
 	 * is written by mbah
-	 * Return: 0
+	 * Return : 0
 	 */
 
 
 	int main(void)
-	{
+
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +19,5 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
 
