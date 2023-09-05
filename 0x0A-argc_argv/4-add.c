a45059b80d3a7d48b9e717c6c8d7adc08fafedea
khalidhub7 <louguikhalid7@gmail.com>
2023-09-05 15:23:41 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,17 +8,17 @@
  * Return:0-success
 */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int sum = 0;
 	char *c;
 
 	while (--argc)
 	{
-		for(c = argv[argc]; *c; c++)
-			if(*c < '0' || *c > '9')
-				return(printf("Error\n"), 1);
-		sum += atoi (argv[argc]);
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum)
 	return (0);
