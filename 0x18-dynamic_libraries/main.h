a615e41bc0e83f6c885fe7a17c1116856f1c1742
khalidhub7 <louguikhalid7@gmail.com>
2023-12-11 02:33:27 -0800

    up

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -25,4 +25,10 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
+int _add(int a, int b);
+int _sub(int a, int b);
+int _mul(int a, int b);
+int _div(int a, int b);
+int _mod(int a, int b);
+
 #endif
