713ba359c3cd548a86c9ae148fd891ce89af23de
khalidhub7 <louguikhalid7@gmail.com>
2023-12-10 22:23:50 -0800

    up

--- 0x18-dynamic_libraries/4.c
+++ 0x18-dynamic_libraries/4.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {}
 char *_strchr(char *s, char c)
---
+++ 0x18-dynamic_libraries/5.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+int _add(int a, int b)
+{}
+int _sub(int a, int b)
+{}
+int _mul(int a, int b)
+{}
+int _div(int a, int b)
+{}
+int _mod(int a, int b)
+{}
