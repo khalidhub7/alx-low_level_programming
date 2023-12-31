81b66acc95794f3db716ef86a15ee19751e4af6d
khalidhub7 <louguikhalid7@gmail.com>
2023-12-30 17:32:07 -0800

    up

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,9 @@
 
 void print_numbers(void)
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+    int i = 0;
+    do {
+        _putchar(i + 48);
+    } while (i >= 0 && 9 >= i);
+    _putchar('\n');
 }
