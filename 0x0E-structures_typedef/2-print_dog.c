b093486714294d54c5078ececec27c42e918b6ce
khalidhub7 <louguikhalid7@gmail.com>
2024-02-28 20:11:44 -0800

    fixing output && output

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,11 +6,11 @@
 */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
-    if (d->name == NULL)
-        d->name = "(nil)";
-    if (d->owner == NULL)
-        d->owner = "(nil)";
-    printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
 }
