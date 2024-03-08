a280180b4adfcfaf0e2f707482ce22515a8a3c8d
khalidhub7 <louguikhalid7@gmail.com>
2024-03-08 02:17:29 -0800

    updating task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,12 +24,12 @@ dog_t *new_dog(char *name, float age, char *owner)
         }
 
     /* Allocate memory for name and owner strings */
-    new_dog_ptr->name = malloc(_strlen(name) + 1);
-    new_dog_ptr->owner = malloc(_strlen(owner) + 1);
+    new_dog_ptr->name = malloc(strlen(name) + 1);
+    new_dog_ptr->owner = malloc(strlen(owner) + 1);
 
     /* Copy name and owner strings */
-    _strcpy(new_dog_ptr->name, name);
-    _strcpy(new_dog_ptr->owner, owner);
+    strcpy(new_dog_ptr->name, name);
+    strcpy(new_dog_ptr->owner, owner);
 
     /* Set the age of the new dog */
     new_dog_ptr->age = age;
