05a06841f62fb72d330185a84a4df8c1d1406e79
khalidhub7 <louguikhalid7@gmail.com>
2024-05-08 07:21:19 -0700

    up

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,22 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#     is header file
+
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+
+int binary_search(int *array, size_t size, int value);
+void printarr(int *array, size_t start, size_t end);
+size_t binary_help(int *array, int value, size_t start, size_t end);
+
+int jump_search(int *array, size_t size, int value);
+
+int interpolation_search(int *array, size_t size, int value);
+size_t prob(int *array, int value, size_t low, size_t high);
+
+int exponential_search(int *array, size_t size, int value);
+
+#endif
