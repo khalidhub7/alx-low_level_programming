e20683e7dd3aa9a17e8f9ac6abdfebc6ab305b8e
khalidhub7 <louguikhalid7@gmail.com>
2024-08-30 19:12:14 -0700

    done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,19 +16,25 @@ void times_table(void)
 		for (b = 0; b < 10; b++)
 		{
 			result = a * b;
-			if (result < 10)
-				_putchar(' ');
-			if (result < 10)
+			if (b == 0)
+			{
 				_putchar(result + '0');
+			}
+			else if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
 			else
 			{
+				_putchar(' ');
 				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
 			if (b < 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
