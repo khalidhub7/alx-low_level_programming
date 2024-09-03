e0e9af40ebeafb1ce42a0edd5c180ea6cf7ab305
khalidhub7 <louguikhalid7@gmail.com>
2024-09-02 20:27:14 -0700

    off

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,42 +1,131 @@
-
-#include <stdio.h>
 #include "main.h"
 
-/*
- * Task 11. 98 Battery Street, the OG
- */
-
 /**
-  * print_to_98 - Beginning/Starting point
-  *
-  * @n: Input Number or argument
-  *
-  * Objectif: Print all natural numbers from n to 98, followed by a new line
-  *
-  * Return: None
-  */
-
+ * print_to_98 - from n to 98
+ *
+ * Return: Always 0 (Success)
+ * Description: prints all natural numbers from n to 98
+ * @n: arg to check
+ */
 void print_to_98(int n)
 {
-	/* Count variable declaration */
-	int run;
+	int i, result = 0;
 
-	if (n < 98)
+	if (n == 0)
 	{
-		for (run = n; run < 98; run++)
+		for (i = 0; i < 99; i++)
 		{
-			printf("%i, ", run);
+			if (result < 10)
+			{
+				_putchar(result + 48);
+			}
+			else if (result >= 10)
+			{
+				_putchar((result / 10) + 48);
+				_putchar((result % 10) + 48);
+			}
+			if (result != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			result++;
 		}
+		_putchar('\n');
+	}
+	else if (n == 98)
+	{
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+		_putchar('\n');
 	}
 	else if (n > 98)
 	{
-		for (run = n; run > 98; run--)
+		while (n > 97)
 		{
-			printf("%i, ", run);
+			if (n < 100)
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+				if (n != 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((n / 100) + 48);
+				_putchar(((n / 10) % 10) + 48);
+				_putchar((n % 10) + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+			n--;
 		}
+		_putchar('\n');
 	}
-	if (run == 98 || n == 98)
+	else if (n < 98 && n > 0)
 	{
-		printf("98\n");
+		while (n != 99)
+		{
+			if (n)
+			{
+				_putchar((n / 10) + 48);
+				_putchar((n % 10) + 48);
+			}
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+		_putchar('\n');
+	}
+	else if (n < 98 && n < 0)
+	{
+		while (n != 99)
+		{
+			if (n < 0)
+			{
+				if ((n / 10) != 0)
+				{
+					n = -n;
+					_putchar('-');
+					_putchar((n / 10) + 48);
+					_putchar((n % 10) + 48);
+					n = -n;
+				}
+				else
+				{
+					n = -n;
+					_putchar('-');
+					_putchar((n % 10) + 48);
+					n = -n;
+				}
+			}
+			else
+			{
+				if ((n / 10) != 0)
+				{
+					_putchar((n / 10) + 48);
+					_putchar((n % 10) + 48);
+				}
+				else
+				{
+					_putchar((n % 10) + 48);
+				}
+			}
+
+			if (n != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			n++;
+		}
+		_putchar('\n');
 	}
 }
