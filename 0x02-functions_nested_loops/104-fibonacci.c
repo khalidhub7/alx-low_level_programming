51471e051716ca8e2eeec80c7350b1155611df00
khalidhub7 <louguikhalid7@gmail.com>
2024-09-19 06:19:09 -0700

    fix part 2

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * fibo_98 - prints the first 98 Fibonacci numbers
+ * fibo_98 - first 98 Fibonacci nums
  *
  * Return: void
  */
 void fibo_98(void)
 {
-	unsigned long int a1 = 1, b1 = 2, sum1;
-	unsigned long int a2 = 0, b2 = 0, sum2;
+	unsigned long int a1 = 1, b1 = 2, sum1 = 0;
+	unsigned long int a2 = 1, b2 = 2, sum2 = 0;
 	int i;
 
 	printf("%lu, %lu", a1, b1);
-
-	for (i = 2; i < 98; i++)
+	for (i = 0; i < 96; i++)
 	{
 		sum1 = a1 + b1;
-		sum2 = a2 + b2;
-		while (sum1 < a1)
+		sum2 = (sum1 < a1 ? 1 : 0) + a2 + b2;
+
+		if (sum1 < a1)
 		{
 			sum2 += 1;
-			/* which is like 4,294,967,295 in 32-bit system */
-			sum1 += 4294967296UL;
 		}
-
 		a1 = b1;
 		a2 = b2;
 		b1 = sum1;
@@ -36,9 +33,8 @@ void fibo_98(void)
 	}
 	printf("\n");
 }
-
 /**
- * main - checks the code
+ * main - check the code
  *
  * Return: Always 0.
  */
