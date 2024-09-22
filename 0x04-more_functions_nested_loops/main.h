81979e30b3706325107451dcfb89c52d43de73c0
khalidhub7 <louguikhalid7@gmail.com>
2024-09-22 16:03:21 -0700

    only putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,17 +3,19 @@
 
 #include <stdio.h>
 
-char _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+void fizz_buzz(void);
+char _putchar(char c);
 int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
 void print_line(int n);
+void more_numbers(void);
+void print_numbers(void);
+void print_number(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void fizz_buzz(void);
 void print_triangle(int size);
+void print_most_numbers(void);
+void prime_factor(unsigned long int n);
 
 #endif
