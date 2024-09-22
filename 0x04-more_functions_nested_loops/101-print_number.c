65c7cdff1315f4164e37345961a5dbf3976f09ce
khalidhub7 <louguikhalid7@gmail.com>
2024-09-22 16:08:09 -0700

    handle INT_MIN

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,12 +6,22 @@
  */
 void print_number(int n)
 {
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
+
 	if (n / 10)
 		print_number(n / 10);
+
 	_putchar((n % 10) + '0');
 }
