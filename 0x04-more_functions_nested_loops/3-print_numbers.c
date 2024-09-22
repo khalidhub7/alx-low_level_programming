4b288086e2053896a7861ffd8b7d461bfeb28407
khalidhub7 <louguikhalid7@gmail.com>
2024-09-21 17:40:48 -0700

    nums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers -  print nums
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
+
