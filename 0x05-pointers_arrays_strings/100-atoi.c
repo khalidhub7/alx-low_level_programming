ccc4b5854477c5bea1a5a0ecd7c6a10d8298d1d3
khalidhub7 <louguikhalid7@gmail.com>
2024-09-25 08:43:59 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ * Return: Always 0
+ */
+int _atoi(char *s)
+{
+	int i = 0, j = 0, sign = 1, num = 0;
+
+	while (s[j] != '\0')
+	{
+		if (s[j] == ' ')
+			j++;
+		else if (s[j] == '-')
+		{
+			sign *= -1;
+			j++;
+		}
+		else if (s[j] == '+')
+            j++;
+		else
+			break;
+	}
+	while (s[i] != '\0')
+	{
+		if (isdigit(s[i]))
+		{
+/* make room of other number hhh */
+			num = num * 10 + (s[i] - '0');
+			if (!isdigit(s[i + 1]))
+				break;
+		}
+		i++;
+	}
+	return (num * sign);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 char _putchar(char c);
 void reset_to_98(int *n);
@@ -15,5 +16,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
